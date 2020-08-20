20b48743173159ebe2127c576e53670ba4e24b70
Beck LaBash <55890162+becklabs@users.noreply.github.com>
2020-08-20 18:07:13 -0400

    Recycled createFrames from zipper.gp_extract, Added splitPath

--- video-toolkit.py
+++ video-toolkit.py
@@ -11,6 +11,7 @@ import dateparser
 import os
 import sys
 import pandas as pd
+import pytz
 
 def getCreationDate(file):
     #GET CREATEDATE FROM EXIFTOOL
@@ -28,11 +29,12 @@ def getCreationDate(file):
     return creationdate
 
 def getOffsets(file):
+    folder,filename = splitPath(file)
     #GET DELTA SECONDS FOR EVERY FRAME
     cap = cv2.VideoCapture(file)
     totalFps = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     offsets = []
-    pbar = tqdm(total=totalFps)
+    pbar = tqdm(total=totalFps,desc='Getting timedeltas for: '+filename,unit='Frames')
     while(cap.isOpened()):
         frame_exists, curr_frame = cap.read()
         if frame_exists:
@@ -42,34 +44,72 @@ def getOffsets(file):
             break
     cap.release()
     pbar.close()
-
+    offsets = [datetime.timedelta(milliseconds=i) for i in offsets]
     return offsets
 
-def getTimestamps(file):
+def splitPath(file):
+    if '\\' in file:
+        file = file.split('\\')
+        filename = file[-1]
+        folder = file[-2]
+    elif '/' in file:
+        file = file.split('/')
+        filename = file[-1]
+        folder = file[-2]
+    else:
+        filename=file
+        folder = ''
+    return folder,filename
+        
+        
+def getTimestamps(file,export=True):
     offsets = getOffsets(file)
     creationdate = getCreationDate(file)
-    
-    #CONVERT OFFSETS TO TIMEDELTAS
-    offsets = [datetime.timedelta(milliseconds=i) for i in offsets]
+    folder, filename = splitPath(file)
     
     #CALCULATE TIMESTAMPS
     timestamps = [creationdate+offset for offset in offsets]
+    timestamps = [timestamp.replace(tzinfo=pytz.UTC) for timestamp in timestamps]
     
     #GENERATE FRAME NAMES
-    frames = [file+'_'+str(i) for i in range(len(timestamps))]
+
+    frames = [filename+'_'+str(i)+'.jpg' for i in range(len(timestamps))]
     
     #EXPORT DATA AS CSV
     df = pd.DataFrame()
     df['Frame'] = frames
     df['Timestamp'] = timestamps
-    df.to_csv(file+'_timestamps.csv')
+    if export == True:
+        df.to_csv(file+'_timestamps.csv')
     return df
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2: 
-        print("Need exactly one argument ...")
-        print("usage: python creationdate.py <filename>")
-        sys.exit()
-    else:
-        file = sys.argv[1]
-        getCreationDate(file)
+def createFrames(file):
+    folder, filename = splitPath(file)
+    if 'frames' not in os.listdir():
+        os.mkdir('frames/')
+    cap = cv2.VideoCapture(file)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    sys.stdout.flush()
+    pbar = tqdm(total=total_frames, unit='Frames',desc='Writing '+str(total_frames)+' frames from ' + filename + ' to frames/')
+    i=0
+    while(cap.isOpened()):
+        frame_exists, frame = cap.read()
+        if frame_exists:
+            cv2.imwrite('frames/'+filename+'_'+ str(i)+'.jpg',frame)
+            i+=1
+            pbar.update(1)
+        else:
+            break
+    pbar.close()
+    cap.release()
+    cv2.destroyAllWindows()
+
+#if __name__ == '__main__':
+    #if len(sys.argv) != 2: 
+        #print("Need exactly one argument ...")
+        #print("usage: python creationdate.py <filename>")
+        #sys.exit()
+    #else:
+        #file = sys.argv[1]
+        #getTimestamps(file)
+    
