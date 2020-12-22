5bab26994b87c580ebdab9da2d8bee28c3b4a220
Beck LaBash <55890162+becklabs@users.noreply.github.com>
2020-12-22 10:45:13 -0500

    Delete gtgui.py

--- gtgui.py
+++
@@ -1,6 +0,0 @@
-from geotag_obj import Project
-
-project = Project()
-project.create(inputPath=r'C:\Users\beck\Documents\CSCR\geotag-tool-python\July31', projectPath=r'C:\Users\beck\Documents\CSCR\geotag-tool-python\July31')
-project.match(timeoffset=4)
-project.export()
